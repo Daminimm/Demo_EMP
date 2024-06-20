a69184b9e9667ad7f264f5d8fce86cb36f389d07
Daminimm <daminim.verve@gmail.com>
2024-06-20 10:39:43 +0530

    admin Dashboard

---
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        private Demo_EmployeeManagementEntities DbContext = new Demo_EmployeeManagementEntities();
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = DbContext.Userlogins.FirstOrDefault(u => u.Username == model.Username && u.Password == model.Password);
+
+                if (user != null)
+                {
+              
+                    if (user.Role == "Admin")
+                    {
+                        return RedirectToAction("AdminDashboard", "Admin");
+                    }
+                    else if (user.Role == "Employee")
+                    {
+                        return RedirectToAction("EmployeeDashboard", "Employee");
+                    }
+                }
+
+                ModelState.AddModelError("", "Invalid username or password.");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ EmployeeManagement/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AdminDashboard()
+        {
+            Demo_EmployeeManagementEntities DbContext = new Demo_EmployeeManagementEntities();
+            List<Employeeinfo> EmployeeList = DbContext.Employeeinfoes.ToList();
+            return View(EmployeeList);
+        
+        }
+        public ActionResult AddEmployee()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddEmployee(Employeeinfo MODEL)
+        {
+            Demo_EmployeeManagementEntities DbContext = new Demo_EmployeeManagementEntities();
+            if (ModelState.IsValid)
+            {
+                DbContext.Employeeinfoes.Add(MODEL);
+                DbContext.SaveChanges();
+                return RedirectToAction("AdminDashboard");
+            }
+
+            return View();
+        }
+
+        
+    }
+}
